9bbb79a751cbb7d04cfcd438133bd6dce31abc3c
huydt96 <dothanhhuy.info@gmail.com>
2022-01-18 12:49:31 +0700

    Update Gradle

--- app/src/main/java/com/example/demo_movie/API.java
+++ app/src/main/java/com/example/demo_movie/API.java
@@ -13,7 +13,7 @@ import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface API {
-    String API_KEY = "8d2cc10d6d30121c3ae4048743b64da3";
+    String API_KEY = "32913c50cf3b822ce755c2d9a29f00eb";
 
     @GET("authentication/token/new?api_key=" + API_KEY)
     @Headers({"Content-Type: application/json;charset=utf-8"})
